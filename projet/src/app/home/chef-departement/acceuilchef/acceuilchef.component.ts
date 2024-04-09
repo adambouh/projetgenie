468f6f9c9787070f0f4535cf2c67bd255aa0ba07
mohammed-kharmichi <mohammedkharmichi@gmail.com>
2024-04-09 13:14:19 +0000

    Discard chef departement content

--- projet/src/app/home/chef-departement/acceuilchef/acceuilchef.component.ts
+++ projet/src/app/home/chef-departement/acceuilchef/acceuilchef.component.ts
@@ -5,31 +5,13 @@ import { ChefDepartementComponent } from '../chef-departement.component';
 @Component({
   selector: 'app-acceuilchef',
   templateUrl: './acceuilchef.component.html',
-  styleUrls: ['./acceuilchef.component.css', './../assets/css/material-dashboard.min.css']
+  styleUrls: ['./acceuilchef.component.css']
 })
 export class AcceuilchefComponent {
 
-  enseignants: any[] = [];
-
-  constructor(private chef_service: ChefDepartementService, private ched_dep: ChefDepartementComponent) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.loadEnseignants(1);
   }
 
-  loadEnseignants(departementId: number) {
-    this.chef_service.getEnseignants(departementId).subscribe(
-      (response: any[]) => {
-        this.enseignants = response;
-
-      },
-      (error) => {
-        console.error('Une erreur s\'est produite : ', error);
-      }
-    );
-  }
-
-  demanderbesoin() {
-    this.ched_dep.demanderBesoinPopUp();
-  }
 }
--- projet/src/app/home/chef-departement/chef-departement.component.ts
+++ projet/src/app/home/chef-departement/chef-departement.component.ts
@@ -6,115 +6,16 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-chef-departement',
   templateUrl: './chef-departement.component.html',
-  styleUrls: ['./chef-departement.component.css', 'assets/css/material-dashboard.min.css'],
+  styleUrls: ['./chef-departement.component.css'],
 })
 export class ChefDepartementComponent {
   
-  notifications: any[] = [];
-  nombreNotificationsNonVues: number = 0;
-
-  formulaire_update_demande_pc!: FormGroup;
-  formulaire_update_demande_imprimante!: FormGroup;
-
-  constructor(private chef_service: ChefDepartementService, private router: Router, private formbuilder: FormBuilder) { 
-    this.formulaire_update_demande_pc = this.formbuilder.group({
-      cpu: [''],
-      ram: [''],
-      disque: [''],
-      ecran: ['']
-    });
-
-    this.formulaire_update_demande_imprimante = this.formbuilder.group({
-      resolution: [''],
-      vitesse: ['']
-    });
-
+  constructor() { 
+   
   }
 
   ngOnInit(): void {
-    this.loadNonSeenNotifications();
+    
   }
   
-  // Fonction pour vérifier si un lien est actif
-  isLinkActive(link: string): boolean {
-    return this.router.url.includes(link);
-  }
-
-  loadNonSeenNotifications() {
-    const chefId = 1; // ID , vous pouvez le définir dynamiquement ou statiquement
-    this.chef_service.getNonSeenNotifications(chefId).subscribe(
-      (response: any[]) => {
-        this.notifications = response;
-        this.nombreNotificationsNonVues = this.notifications.length;
-      },
-      (error) => {
-        console.error('Une erreur s\'est produite : ', error);
-      }
-    );
-  }
-
-  
-
-  // Référence à l'élément pop-up dans le template HTML
-  @ViewChild('popUpUpdateDemande_pc')
-  popUpUpdateDemande_pc!: ElementRef;
-
-  @ViewChild('popUpUpdateDemande_imprimante')
-  popUpUpdateDemande_imprimante!: ElementRef;
-
-  @ViewChild('demander_besoins')
-  demander_besoins!: ElementRef;
-
-  // Fonction pour masquer le pop-up
-  removeUpdateDemandePopUp() {
-    // Vérifier si l'élément existe
-    if (this.popUpUpdateDemande_pc) {
-      // Rendre l'élément invisible
-      this.popUpUpdateDemande_pc.nativeElement.style.display = 'none';
-    }
-
-    // Vérifier si l'élément existe
-    if (this.popUpUpdateDemande_imprimante) {
-      // Rendre l'élément invisible
-      this.popUpUpdateDemande_imprimante.nativeElement.style.display = 'none';
-    }
-
-    // Vérifier si l'élément existe
-    if (this.demander_besoins) {
-      // Rendre l'élément invisible
-      this.demander_besoins.nativeElement.style.display = 'none';
-    }
-  }
-
-  updateDemandePopUp(demande: any) {
-   if(demande.type == "pc") {
-    this.formulaire_update_demande_pc.patchValue({
-      cpu: demande.cpu,
-      ram: demande.ram,
-      disque: demande.disque,
-      ecran: demande.ecran === 1 ? 'Avec' : 'Sans'
-    });
-    console.log(demande.cpu);
-    console.log(demande.ram);
-    console.log(demande.disque);
-    console.log(demande.ecran);
-    
-    this.popUpUpdateDemande_pc.nativeElement.style.display = 'flex';
-   } else if(demande.type == "imprimante"){
-    this.formulaire_update_demande_imprimante.patchValue({
-      resolution: demande.resolution,
-      vitesse: demande.vitesse,
-    });
-    
-    this.popUpUpdateDemande_imprimante.nativeElement.style.display = 'flex';
-   }
-  }
-
-  demanderBesoinPopUp() {
-    // Vérifier si l'élément existe
-    if (this.demander_besoins) {
-      // Rendre l'élément invisible
-      this.demander_besoins.nativeElement.style.display = 'flex';
-    }
-  }
 }
