43ae45833950d93fecb9b865f4bc4eb1975d60aa
AyaFELLAH <130109272+AyaFELLAH@users.noreply.github.com>
2024-04-30 13:20:44 +0100

    ajout service getDetailByProposition

--- backend_gestion_ressource/src/main/java/gestionRessource/backend/service/Impl/DetailServiceImpl.java
+++ backend_gestion_ressource/src/main/java/gestionRessource/backend/service/Impl/DetailServiceImpl.java
@@ -1,10 +1,15 @@
 package gestionRessource.backend.service.Impl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import gestionRessource.backend.model.Detail;
+import gestionRessource.backend.model.Proposition;
 import gestionRessource.backend.repository.DetailRepository;
+import gestionRessource.backend.repository.PropositionRepository;
 import gestionRessource.backend.service.DetailService;
 
 @Service
@@ -12,8 +17,18 @@ public class DetailServiceImpl implements DetailService {
 	@Autowired
 	private DetailRepository detailRepository;
 
+	@Autowired
+	private PropositionRepository propositionRepository;
+
 	@Override
 	public Detail saveDetail(Detail detail) {
 		return detailRepository.save(detail);
+	}
+
+	@Override
+	public List<Detail> getDetailByProposition(Long propositionId) {
+		Optional<Proposition> propositionOptional = propositionRepository.findById(propositionId);
+		Proposition proposition = propositionOptional.get();
+		return detailRepository.findByProposition(proposition);
 	}
 }
